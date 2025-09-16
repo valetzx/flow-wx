9580beee0b238facd405d9379c1e0d8571dd0a9f
valetzx <45054400+valetzx@users.noreply.github.com>
2025-09-16 16:03:24 +0800

    add clarity

--- static/common.js
+++ static/common.js
@@ -100,6 +100,13 @@ window.commonReady = new Promise(resolve => {
     initDarkMode();
     setupSplash();
     setupTagListDrag();
+    
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tbkb63wh98");
+    
     resolve();
   });
 });
